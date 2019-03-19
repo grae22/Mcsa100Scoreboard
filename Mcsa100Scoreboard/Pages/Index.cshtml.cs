2a237886e54723539a840a1cd21751d062b96032
GraemeB <graeme.bruschi@derivco.co.za>
2019-03-19 08:40:35 +0000

    Moving scoreboard -> index.

--- Mcsa100Scoreboard/Pages/Index.cshtml.cs
+++ Mcsa100Scoreboard/Pages/Index.cshtml.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+
+using Mcsa100Scoreboard.Domain;
+using Mcsa100Scoreboard.Models;
+using Mcsa100Scoreboard.Services;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mcsa100Scoreboard.Pages
 {
   public class IndexModel : PageModel
   {
-    public void OnGet()
-    {
+    // TODO: Remove.
+    private const string ApiKey = "AIzaSyAYhi3At01IDR5rbEIM8XSXruvgk-5NDMU";
 
+    private readonly IGoogleSheetService _googleSheetService;
+
+    public IndexModel(IGoogleSheetService googleSheetService)
+    {
+      _googleSheetService = googleSheetService ?? throw new ArgumentNullException(nameof(googleSheetService));
+    }
+
+    public async Task OnGet()
+    {
+      var address = new Uri($"https://sheets.googleapis.com/v4/spreadsheets/1qYBulO5nLpFs574h49En8POnvQjXcdVB6VpU1gMBIQQ/values/Sheet1!A1:Z500?key={ApiKey}");
+      var input = await _googleSheetService.RetrieveInput<InputModel>(address);
+      var scoreboard = new Scoreboard(input);
     }
   }
 }
