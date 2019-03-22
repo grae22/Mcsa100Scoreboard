611a178f6897b1de71950e961dd7e4fa4dd2275e
GraemeB <grae22@gmail.com>
2019-03-22 09:10:56 +0000

    Added countdown stuff, but... not prob gonna use it.

---
+++ Mcsa100Scoreboard/Domain/CountdownBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace Mcsa100Scoreboard.Domain
+{
+  internal class CountdownBuilder
+  {
+    public static string Build(in DateTime end, in DateTime now)
+    {
+      if (end < now)
+      {
+        return string.Empty;
+      }
+
+      TimeSpan timeDifference = end - now;
+
+      int monthsRemaining = (int)timeDifference.TotalDays / 30;
+      int weeksComponent = (int)timeDifference.TotalDays % 30 / 7;
+      int daysComponent = (int)timeDifference.TotalDays % 30 % 7;
+
+      var stringBuilder = new StringBuilder();
+
+      if (monthsRemaining > 0)
+      {
+        stringBuilder.Append($"{monthsRemaining}");
+        stringBuilder.Append(monthsRemaining > 1 ? " months, " : " month, ");
+      }
+
+      if (weeksComponent > 0)
+      {
+        stringBuilder.Append($"{weeksComponent}");
+        stringBuilder.Append(weeksComponent > 1 ? " weeks, " : " week, ");
+      }
+
+      if (daysComponent > 0)
+      {
+        stringBuilder.Append($"{daysComponent}");
+        stringBuilder.Append(daysComponent > 1 ? " days" : " day");
+      }
+
+      string result = stringBuilder.ToString().Trim();
+
+      if (result.EndsWith(','))
+      {
+        result = result.Substring(0, result.Length - 1);
+      }
+
+      return result;
+    }
+  }
+}
---
+++ Tests/Domain/CountdownBuilderTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+using Mcsa100Scoreboard.Domain;
+
+using NUnit.Framework;
+
+namespace Tests.Domain
+{
+  [TestFixture]
+  public class CountdownBuilderTests
+  {
+    [Test]
+    [TestCase("2020/1/1", "2019/1/1", "12 months", null)]
+    [TestCase("2020/1/1", "2019/12/1", "1 month", "months")]
+    [TestCase("2020/1/1", "2019/12/3", null, "month")]
+    public void Build_GivenDate_ShouldReturnCorrectMonthsRemainingValue(
+      in string end,
+      in string now,
+      in string expected,
+      in string notExpected)
+    {
+      // Arrange.
+      // Act.
+      var result = CountdownBuilder.Build(DateTime.Parse(end), DateTime.Parse(now));
+
+      // Assert.
+      if (expected != null)
+      {
+        StringAssert.Contains(expected, result);
+      }
+
+      if (notExpected != null)
+      {
+        StringAssert.DoesNotContain(notExpected, result);
+      }
+    }
+
+    [Test]
+    [TestCase("2020/1/1", "2019/1/16", "2 weeks", null)]
+    [TestCase("2020/1/1", "2019/1/14", "3 weeks", null)]
+    [TestCase("2020/1/1", "2019/1/30", null, "week")]
+    public void Build_GivenDate_ShouldReturnCorrectWeeksRemainingValue(
+      in string end,
+      in string now,
+      in string expected,
+      in string notExpected)
+    {
+      // Arrange.
+      // Act.
+      var result = CountdownBuilder.Build(DateTime.Parse(end), DateTime.Parse(now));
+
+      // Assert.
+      if (expected != null)
+      {
+        StringAssert.Contains(expected, result);
+      }
+
+      if (notExpected != null)
+      {
+        StringAssert.DoesNotContain(notExpected, result);
+      }
+    }
+
+    [Test]
+    [TestCase("2020/1/1 00:00:00", "2019/12/30 12:00:00", "1 day", null)]
+    public void Build_GivenDate_ShouldReturnCorrectDaysRemainingValue(
+      in string end,
+      in string now,
+      in string expected,
+      in string notExpected)
+    {
+      // Arrange.
+      // Act.
+      var result = CountdownBuilder.Build(DateTime.Parse(end), DateTime.Parse(now));
+
+      // Assert.
+      if (expected != null)
+      {
+        StringAssert.Contains(expected, result);
+      }
+
+      if (notExpected != null)
+      {
+        StringAssert.DoesNotContain(notExpected, result);
+      }
+    }
+
+    [Test]
+    [TestCase("2020/1/1 00:00:00", "2019/11/23 12:00:00", "1 month, 1 week, 1 day", null)]
+    public void Build_GivenDate_ShouldReturnCorrectRemainingValue(
+      in string end,
+      in string now,
+      in string expected,
+      in string notExpected)
+    {
+      // Arrange.
+      // Act.
+      var result = CountdownBuilder.Build(DateTime.Parse(end), DateTime.Parse(now));
+
+      // Assert.
+      if (expected != null)
+      {
+        StringAssert.Contains(expected, result);
+      }
+
+      if (notExpected != null)
+      {
+        StringAssert.DoesNotContain(notExpected, result);
+      }
+    }
+  }
+}
