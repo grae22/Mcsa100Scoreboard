2a7dfc6d0c36d89367dc68800f5086ab2f636911
GraemeB <grae22@gmail.com>
2019-05-09 15:58:40 +0000

    Initial 'scoreable' implementation.

---
+++ Mcsa100Scoreboard/Domain/Hikes/Scoreable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace Mcsa100Scoreboard.Domain.Hikes
+{
+  public class Scoreable
+  {
+    private static readonly char[] ValidTypes = { 'S', 'C', 'P' };
+
+    public static Scoreable Create(in string rawName)
+    {
+      char? scoreableTypeId = GetScoreableType(rawName);
+
+      if (scoreableTypeId == null)
+      {
+        return null;
+      }
+
+      return new Scoreable(
+        scoreableTypeId.Value,
+        "");
+    }
+
+    private static char? GetScoreableType(in string rawName)
+    {
+      if (rawName == null ||
+          rawName.Length < 3)
+      {
+        return null;
+      }
+
+      string prefixWithTypeId =
+        rawName
+          .Substring(0, 3)
+          .ToUpper();
+
+      char id = prefixWithTypeId[1];
+
+      if (!IsValidScoreableTypeId(id))
+      {
+        return null;
+      }
+
+      return id;
+    }
+
+    private static bool IsValidScoreableTypeId(in char id)
+    {
+      return ValidTypes.Contains(id);
+    }
+
+    public char TypeId { get; }
+    public string Name { get; }
+
+    private Scoreable(
+      in char typeId,
+      in string name)
+    {
+      TypeId = typeId;
+      Name = name ?? throw new ArgumentNullException(nameof(typeId));
+    }
+  }
+}
---
+++ Tests/Domain/Hikes/ScoreableTests.cs
@@ -0,0 +1,42 @@
+﻿using Mcsa100Scoreboard.Domain.Hikes;
+
+using NUnit.Framework;
+
+namespace Tests.Domain.Hikes
+{
+  [TestFixture]
+  public class ScoreableTests
+  {
+    [TestCase("(S)")]
+    [TestCase("(P)")]
+    [TestCase("(C)")]
+    public void Create_GivenCorrectTypeId_ShouldReturnScoreable(in string name)
+    {
+      // Arrange.
+      // Act.
+      Scoreable result = Scoreable.Create(name);
+
+      // Assert.
+      Assert.NotNull(result);
+      Assert.AreEqual(name[1], result.TypeId);
+    }
+
+    [TestCase("(X)")]
+    [TestCase("(X")]
+    [TestCase("X)")]
+    [TestCase("X")]
+    [TestCase("S")]
+    [TestCase("C")]
+    [TestCase("P")]
+    [TestCase(null)]
+    public void Create_GivenInvalidTypeId_ShouldReturnNull(in string name)
+    {
+      // Arrange.
+      // Act.
+      Scoreable result = Scoreable.Create(name);
+
+      // Assert.
+      Assert.Null(result);
+    }
+  }
+}
