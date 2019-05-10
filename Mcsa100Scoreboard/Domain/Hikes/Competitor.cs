fb66f29eef5e512cc35917413375a9fe81affd83
GraemeB <grae22@gmail.com>
2019-05-10 06:26:15 +0000

    Competitor added with basic logic.

---
+++ Mcsa100Scoreboard/Domain/Hikes/Competitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mcsa100Scoreboard.Domain.Hikes
+{
+  public class Competitor
+  {
+    public string Name { get; }
+    public IEnumerable<Scoreable> Scoreables => _scoreables;
+
+    private List<Scoreable> _scoreables = new List<Scoreable>();
+
+    public Competitor(in string name)
+    {
+      if (string.IsNullOrWhiteSpace(name))
+      {
+        throw new ArgumentException($"'{nameof(name)}' cannot be null or empty.");
+      }
+
+      Name = name;
+    }
+
+    public void AddScoreable(in string text)
+    {
+      Scoreable newScoreable = Scoreable.Create(text);
+
+      if (newScoreable == null)
+      {
+        return;
+      }
+
+      _scoreables.Add(newScoreable);
+    }
+  }
+}
---
+++ Tests/Domain/Hikes/CompetitorTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using Mcsa100Scoreboard.Domain.Hikes;
+
+using NUnit.Framework;
+
+namespace Tests.Domain.Hikes
+{
+  [TestFixture]
+  public class CompetitorTests
+  {
+    [Test]
+    public void AddScoreable_GivenValidScoreable_ShouldBePresentInScoreablesProperty()
+    {
+      // Arrange.
+      var testObject = new Competitor("SomeName");
+
+      // Act.
+      testObject.AddScoreable("(S) Some Summit");
+
+      // Assert.
+      Assert.AreEqual(1, testObject.Scoreables.Count(s => s.Name == "Some Summit"));
+    }
+  }
+}
